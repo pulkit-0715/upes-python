e8b452c570070f4c8dc660c4fd1795664417d648
pulkit-0715 <singhpulkit1506@gmail.com>
2026-01-15 22:54:03 +0530

    unit 1

---
+++ simpleintrest.py
@@ -0,0 +1,11 @@
+print(f"Enter principal,rate of intrest and time")
+p=int(input(""))
+r=int(input(""))
+t=int(input(""))
+
+si=p*r*t/100
+
+amt=p+si
+
+print(f"Simple intrest: {si}")
+print(f"amount {amt}")
