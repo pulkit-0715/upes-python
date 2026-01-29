fd0470e52f1982a3bdae5d46515ca71794493a26
pulkit-0715 <singhpulkit1506@gmail.com>
2026-01-29 09:04:27 +0530

    Remove deleted lecture files

--- Experiment2/greatestamong3.py
+++
@@ -1,8 +0,0 @@
-import math
-a=int(input("Enter 1st num"))
-b=int(input("Enter 2nd num"))
-c=int(input("Enter 3rd num"))
-
-print(f"Max among 3:\t{max(a,b,c)}")
-print(f"Min among 3:\t{min(a,b,c)}")
-print(f"Middel among 3:\t{(a+b+c)-(max(a,b,c) + min(a,b,c))}")
