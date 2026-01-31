50cb0d4b08b38152bfb5e05bfe12095b1389f6a7
pulkit-0715 <singhpulkit1506@gmail.com>
2026-01-31 21:41:03 +0530

    Expriment 1

--- Experiment1/sequence.py
+++ Experiment1/sequence.py
@@ -1,12 +1,8 @@
-'''
-To find a numbein an array
-'''
+'''To find a numbein an array'''
 seq = (10, 20, 56, 78, 89)
 
 num = int(input("Enter the number: "))
-
 found = False
-
 for x in seq:
     if x == num:
         found = True
